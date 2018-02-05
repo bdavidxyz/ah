90cbca5928e9e38677a5a5d8f70123922eb79316
David Boureau <bdavidxyz@gmail.com>
2018-02-05 21:36:39 +0100

    add a final stuff

---
+++ app/controllers/final_controller.rb
@@ -0,0 +1,4 @@
+class FinalController < ApplicationController
+  def index    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'final/index'
  
   root 'welcome#index'
 end
