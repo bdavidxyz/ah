3637d8f0903d03fbb728ccaf6f40d7d2881f6b5e
David Boureau <bdavidxyz@gmail.com>
2018-02-08 22:05:56 +0100

    one unuseful stuff

---
+++ app/view_objects/test_table_row.rb
@@ -0,0 +1,20 @@
+class TestTableRow < ViewObject
+
+  attr_reader :position, :test_function, :test_expectation
+
+  def after_init(args)
+    locals = hash_for(args)
+    @position = string_for(args[:index])
+    @test_function = string_for(args[:test_function])
+    @test_expectation = string_for(args[:test_expectation])
+  end
+
+  def badge_css
+  end
+
+  def badge_text
+  end
+
+  def _is_ok
+  end
+end
