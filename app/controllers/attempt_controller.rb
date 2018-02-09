e8b2fa31d4cef2930babe5e2ffff012afeedce73
David Boureau <bdavidxyz@gmail.com>
2018-02-09 15:30:00 +0100

    cleaned, ok

--- app/controllers/attempt_controller.rb
+++
@@ -1,4 +0,0 @@
-class AttemptController < ApplicationController
-  def index    
-  end
-end
--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -2,10 +2,12 @@ class QuestionController < ApplicationController
 
   def facile    
     q = Question.first
-    render locals: q.attributes.symbolize_keys
+    render 'question/show', locals: q.attributes.symbolize_keys
   end
 
   def difficile   
+    q = Question.first
+    render 'question/show', locals: q.attributes.symbolize_keys
   end
 end
 
