5fb4e5f91190058e3958442a0d4145e376b3dcfe
David Boureau <bdavidxyz@gmail.com>
2018-02-14 21:51:56 +0100

    form

--- app/forms/attempt_form.rb
+++ app/forms/attempt_form.rb
@@ -1,6 +1,6 @@
 class AttemptForm
   include ActiveModel::Model
 
-  attr_accessor :biz_id, :functionf, :nb_of_lint_warning, :nb_of_green_test, :nb_of_green_test
+  attr_accessor :biz_id, :functionf, :nb_of_lint_warning, :nb_of_green_test, :nb_of_red_test
 
 end
