2b977938d042704fe7c14cf21e941ec5f7147d8d
David Boureau <bdavidxyz@gmail.com>
2018-02-22 14:37:25 +0100

    trying a first test

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,6 +1,10 @@
 require "rails_helper"
 
 RSpec.feature "Welcome page", :type => :feature do
+  before(:each) do
+    create(:question, :easy)    
+    create(:question, :hard)    
+  end
   scenario "User is on the welcome page by default" do
     visit root_path
     expect(page).to have_css("c-home-jumbotron")
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,8 @@ require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 # Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
+
+
 RSpec.configure do |config|
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
