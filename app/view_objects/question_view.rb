189af64754bf1238cecb8983d91d21f6d32e27d9
David Boureau <bdavidxyz@gmail.com>
2018-02-22 21:54:00 +0100

    cleaned

--- app/view_objects/question_view.rb
+++ app/view_objects/question_view.rb
@@ -3,12 +3,12 @@ class QuestionView < ViewObject
   attr_reader :functionf, :done, :question, :initial, :description, :tests
 
   def after_init(args)
-    locals = hash_for(args)
-    @functionf  = string_for(locals[:functionf])
-    @done  = boolean_for(locals[:done])
-    @question  = hash_for(locals[:question])
-    @tests  = json_for(locals[:tests])
-    @initial  = string_for(question[:initial])
+    locals        = hash_for(args)
+    @functionf    = string_for(locals[:functionf])
+    @done         = boolean_for(locals[:done])
+    @question     = hash_for(locals[:question])
+    @tests        = json_for(locals[:tests])
+    @initial      = string_for(question[:initial])
     @description  = string_for(question[:description]).html_safe
   end
 
