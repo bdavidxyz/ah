c075f7a2c1ec9fa177aea4076e6f03686034fe65
David Boureau <bdavidxyz@gmail.com>
2018-02-22 15:27:17 +0100

    cleaned controller

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,14 +1,26 @@
 class QuestionController < ApplicationController
 
   def show
-    q = Question.find_by!(biz_id: params[:id])
-    attempt_biz_id = params[:for_id]
-    att = Attempt.find_or_create_by(biz_id: attempt_biz_id, question_id: q.id)
+    question = find_question_or_fail(params[:id])
+    attempt = find_or_create_attempt(params[:for_id], question)
+    render locals: hydrate_view(question, attempt)
+  end
+
+private
+  def find_question_or_fail(id)
+    Question.find_by!(biz_id: id)
+  end
+
+  def find_or_create_attempt(biz_id, question)
+    Attempt.find_or_create_by(biz_id: biz_id, question_id: question.id)
+  end
+
+  def hydrate_view(question, attempt)
     h_view = {}
-    h_view[:question] = q.attributes.symbolize_keys
-    h_view[:functionf] = att.functionf
-    h_view[:done] = att.status == "done"
-    render locals: h_view
+    h_view[:question] = question.attributes.symbolize_keys
+    h_view[:functionf] = attempt.functionf
+    h_view[:done] = attempt.status == "done"
+    h_view
   end
 
 end
