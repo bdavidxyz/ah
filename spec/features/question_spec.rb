a6772601093290a744e58107f50e4d4621a016d2
David Boureau <bdavidxyz@gmail.com>
2018-02-22 22:01:02 +0100

    added valid tests

--- spec/features/question_spec.rb
+++ spec/features/question_spec.rb
@@ -19,21 +19,26 @@ RSpec.feature "Question page", :type => :feature do
     scenario "Display form fields" do
       expect(page).to have_css(".qa-form-fields")
     end    
+    scenario "Do not display info that test is done" do
+      expect(page).not_to have_css(".qa-info-done")
+    end    
   end
 
   context 'Existing attempt' do
     before(:each) do
       question = create(:question, :easy)    
       attempt = create(:attempt, question: question, status: "done", biz_id: fresh_and_new_attempt_id)    
-      visit question_path(question.biz_id)
+      visit question_path(question.biz_id, for_id: attempt.biz_id)
     end
     scenario "Do not display validation button" do
-      expect(page).to have_css(".qa-validate")
+      expect(page).not_to have_css(".qa-validate")
     end    
     scenario "Do not display form fields" do
-      expect(page).to have_css(".qa-form-fields")
+      expect(page).not_to have_css(".qa-form-fields")
     end    
-    
+    scenario "Display info that test is done" do
+      expect(page).to have_css(".qa-info-done")
+    end        
   end
 
   private 
