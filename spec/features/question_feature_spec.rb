072bdf29cc96da78093744985b14f9e0354e0f12
David Boureau <bdavidxyz@gmail.com>
2018-02-23 17:00:07 +0100

    cleaner code

--- spec/features/question_feature_spec.rb
+++ spec/features/question_feature_spec.rb
@@ -3,67 +3,66 @@ require "rails_helper"
 RSpec.feature "Question page", :type => :feature do
 
   context 'New attempt' do
-    that = nil
+    seen = nil
     before do
-      if !that
+      if !seen
         question = create(:question, :easy)    
         visit question_path(question.biz_id)
-        that = Nokogiri::HTML(page.html)
+        seen = Nokogiri::HTML(page.html)
       end
     end
     scenario "Display an enabled editor" do
-      expect(that.css('.enabled-editor').size).to eq(1)
+      should_have seen, 1, '.enabled-editor'
     end    
     scenario "Display code editor" do
-      expect(that.css('#editor').size).to eq(1)
+      should_have seen, 1, '#editor'
     end    
     scenario "Display test table" do
-      expect(that.css('.c-table').size).to eq(1)
+      should_have seen, 1, '.c-table'
     end    
     scenario "Display validation button" do
-      expect(that.css('.qa-validate').size).to eq(1)
+      should_have seen, 1, '.qa-validate'
     end    
     scenario "Display form fields" do
-      expect(that.css('.qa-form-fields').size).to eq(1)
+      should_have seen, 1, '.qa-form-fields'
     end    
     scenario "Do not display info that test is done" do
-      expect(that.css('.qa-info-done').size).to eq(0)
+      should_have seen, 0, '.qa-info-done'
     end    
   end
 
 
   context 'Existing attempt' do
-    that = nil
+    seen = nil
     before do
-      if !that
+      if !seen
         question = create(:question, :easy)    
         attempt = create(:attempt, question: question, status: "done", biz_id: fresh_and_new_attempt_id)    
         visit question_path(question.biz_id, for_id: attempt.biz_id)
-        that = Nokogiri::HTML(page.html)
+        seen = Nokogiri::HTML(page.html)
       end
     end
     scenario "Display an disabled editor" do
-      expect(that.css('.disabled-editor').size).to eq(1)
+      should_have seen, 1, '.disabled-editor'
     end    
     scenario "Do not display validation button" do
-      expect(that.css('.qa-validate').size).to eq(0)
+      should_have seen, 0, '.qa-validate'
     end
     scenario "Do not display form fields" do
-      expect(that.css('.qa-form-fields').size).to eq(0)
+      should_have seen, 0, '.qa-form-fields'
     end
     scenario "Display info that test is done" do
-      expect(that.css('.qa-info-done').size).to eq(1)
+      should_have seen, 1, '.qa-info-done'
     end
     scenario "Display test table" do
-      # expect(that.css('.c-table').size).to eq(1)
-      should_have(that, '.c-table', 1)
+      should_have seen, 1, '.c-table'
     end    
   end
 
   private 
 
-  def should_have(that, selector, size)
-    expect(that.css(selector).size).to eq(size)
+  def should_have(seen, size, selector)
+    expect(seen.css(selector).size).to eq(size)
   end
 
   def fresh_and_new_attempt_id
