5b292d53454eeac3067a62de4725fb889c0a9350
David Boureau <bdavidxyz@gmail.com>
2018-03-02 18:07:11 +0100

    first oauth attempt

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -7,9 +7,11 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_admin
+    helper_method :current_user
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      return if ENV['AH_SKIP_ADMIN_AUTH']
+      redirect_to signin_path unless current_user
     end
 
     # Override this value to specify the number of elements to display at a time
@@ -17,5 +19,12 @@ module Admin
     # def records_per_page
     #   params[:per_page] || 20
     # end
+
+    private
+    
+    def current_user
+      session[:user_email]
+    end
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,12 @@ Rails.application.routes.draw do
   resources :question, only: [:show]
   resources :final, only: [:create, :index]
 
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/signin' => 'sessions#new', :as => :signin
+  get '/signout' => 'sessions#destroy', :as => :signout
+  get '/auth/failure' => 'sessions#failure'
+  get '/auth/google_oauth2', as: :google_oauth
+  
   get 'welcome/index' 
   root 'welcome#index'
 end
